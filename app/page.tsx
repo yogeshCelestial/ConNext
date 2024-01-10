86f054f3be58b31646a3f315c98182860dcccc93
yogesh <yogesh@celestialsys.com>
2024-01-10 06:58:55 +0530

    prisma introduced with psqldb on neon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
-import styles from './page.module.css'
+import styles from './page.module.css';
 
-export default function Home() {
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  // const allUsers = await prisma.user.findMany();
+
+    // const user1 = await prisma.user.create({
+    //   data: {
+    //     name: 'Alice',
+    //     email: 'alice@prisma.io',
+    //   }
+    // })
+  // return user1;
+}
+
+export default async function Home() {
+  const data = await main();
+  console.log(data);
+
   return (
     <main className={styles.main}>
       <h1>Hello From ConNext</h1>     
