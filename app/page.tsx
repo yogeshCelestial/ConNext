a7997a38b507abe2d094dfad5795d8f3fae2f9a9
yogesh <yogesh@celestialsys.com>
2024-01-14 09:54:17 +0530

    home page, single post component are now dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,23 @@
 'use client';
 import Main from '@/components/Main';
-
-const mockData = [
-  {
-    id: 1,
-    title: "And the Ship Sails On (E la nave va)",
-    description: "Acute embolism and thrombosis of deep vein of unsp low extrm Acute embolism and thrombosis of deep vein of unsp low extrm Acute embolism and thrombosis of deep vein of unsp low extrm Acute embolism and thrombosis of deep vein of unsp low extrm",
-    image: 'https://picsum.photos/400/400/'
-  },
-  {
-    id: 2,
-    title: "Chantilly the Ship Sails On (E la nave va Lace",
-    description: "Nondisp artic fx Acute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrm head of r femr, 7thN",
-    image: 'https://picsum.photos/400/400/'
-  },
-  {
-    id: 3,
-    title: "Mystery Team is on the way to heaven",
-    description: "Fracture of manubrium, Acute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrm initial encounter for closed fracture",
-    image: 'https://picsum.photos/400/400/'
-  },
-  {
-    id: 4,
-    title: "Airplane II: The Sequel of the Endgame",
-    description: "Matern care for known or Acute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrm susp placntl insuff, 1st tri, fts5",
-    image: 'https://picsum.photos/400/400/'
-  },
-  {
-    id: 5,
-    title: "Sleepwalkers are next dinasours",
-    description: "Laceration of Acute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrmAcute embolism and thrombosis of deep vein of unsp low extrm axillary or brachial vein, left side, sequela",
-    image: 'https://picsum.photos/400/400/'
-  }
-]
-
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const loadPosts = async () => {
+    const response = await fetch('/api/post', {
+      method: 'GET'
+    });
+    const data = await response.json();
+    if (data?.posts?.length) {
+      setPosts(data?.posts);
+    }
+  }
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
   return (
-    <Main mockData={mockData} category='Trending' />
+    <Main posts={posts} category='Trending' />
   )
 }
--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useParams } from 'next/navigation';
 import EditIcon from '@mui/icons-material/Edit';
@@ -8,49 +8,62 @@ import './singlePost.css';
 import Paper from '@mui/material/Paper';
 import CardComp from '../../../components/Card';
 import Link from 'next/link';
+import { Avatar } from '@mui/material';
+import { useSession } from 'next-auth/react';
 
+export interface SnglPost {
+  id: string,
+  title: string,
+  content: string,
+  image: string,
+  createdAt: string,
+}
 const SinglePost = () => {
   const params = useParams();
+  const { data: session } = useSession();
+  const [singlePost, setSinglePost] = useState<SnglPost>();
+  const [postedTime, setPostedTime] = useState(NaN);
   const { postId } = params;
+  const getSinglePost = async () => {
+    const response = await fetch(`/api/post/${postId}`, {
+      method: 'GET'
+    });
+    const postData = await response.json();
+    setSinglePost(postData?.post)
+  }
+
+  useEffect(() => {
+    getSinglePost();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [postId]);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const convertedDate = new Date(singlePost?.createdAt!);
+    // Calculate the difference in milliseconds
+    const timeDifference: number = currentDate.getTime() - convertedDate.getTime();
+
+    // Convert the time difference to days
+    const daysAgo: number = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    setPostedTime(daysAgo);
+  }, [singlePost]);
+
   return (
     <div className='single'>
       <div className='content'>
-        <Image className='postImage' loading='lazy' src="https://picsum.photos/400/600/" alt='post' height={400} width={500} />
+        {/* <Image className='postImage' loading='lazy' src={singlePost?.image!} alt='post' height={400} width={500} /> */}
         <div className='userDiv'>
-          <Image loading='lazy' className='user' src='https://picsum.photos/50/50/' alt='user' height={50} width={50} />
+          <Avatar alt={session?.user?.name!} src={session?.user?.image!} />
           <div className='info'>
             <span>Yogesh</span>
-            <p>Posted 2 days ago</p>
+            <p>Posted {postedTime === 0 ? 'today' : (postedTime === 1) ? 'day ago' : 'days ago'}</p>
           </div>
           <Link href='/write'><EditIcon /></Link>
           <DeleteOutlineIcon />
         </div>
         <div>
-          <h1>This is the title of the Blog Post in the Single Page</h1>
-          <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed tincidunt, velit sit amet hendrerit vulputate, lacus felis
-            hendrerit urna, id sagittis dolor quam eu nisi. In fermentum,
-            tortor quis ultrices varius, mauris lacus elementum nunc, quis
-            faucibus metus quam vel sem. Morbi eu tincidunt dolor. Duis vel
-            fringilla tortor, sit amet convallis libero. Fusce in felis
-            a ex ultricies pellentesque vel vel ligula. Suspendisse potenti
-            Proin vitae vestibulum odio. Ut auctor velit vel lacus
-            pellentesque, eget facilisis dolor auctor.
-            Vestibulum tincidunt libero sit amet urna sodales, in feugiat justo malesuada.
-            Praesent euismod, enim et auctor tincidunt, orci dui hendrerit justo,
-            vitae lacinia leo quam eu neque. Sed semper felis ac odio fermentum,
-            ut dignissim tortor pulvinar. Quisque eget turpis eu justo accumsan ultricies.
-            Nullam luctus justo id feugiat eleifend. Integer vel mi vitae leo
-            imperdiet fermentum at quis justo. Curabitur quis turpis nec felis dapibus malesuada.
-            Fusce bibendum semper tortor, a tincidunt nunc fermentum nec.
-            Curabitur dapibus, odio nec scelerisque congue, quam nunc bibendum lectus,
-            id dictum mauris orci ut odio. Aliquam id aliquet odio. Ut tristique,
-            quam ut volutpat congue, erat justo hendrerit nulla, sit amet dictum ex nulla eget libero.
-            Vestibulum nec justo eget ligula feugiat rhoncus. Etiam euismod accumsan est.
-            Integer vel augue vel est gravida ultrices. Sed in leo id urna cursus congue eget vitae libero.
-            Vivamus in enim eu justo iaculis finibus. Integer consectetur urna in neque malesuada efficitur.
-            Suspendisse potenti. Phasellus ullamcorper, tortor nec tristique hendrerit, velit tellus euismod eros,
-            a varius mi dolor nec nisi.</p>
+          <h1>{singlePost?.title}</h1>
+          <p className='description'>{singlePost?.content}</p>
         </div>
       </div>
       <div className='menu'>
