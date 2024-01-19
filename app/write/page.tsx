1296907cebfeab44b6f517a32223ef05c0e5e4f8
yogesh <yogesh@celestialsys.com>
2024-01-19 16:47:16 +0530

    snackbar notification when post saved

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -4,11 +4,10 @@ import './write.css';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import SelectCategory from '../../components/SelectCategory';
-import { Button, Paper } from '@mui/material';
+import { Alert, AlertColor, Button, Paper, Snackbar } from '@mui/material';
 import { useLayoutEffect } from 'react';
 import { signIn, useSession } from 'next-auth/react';
 import { useEdgeStore } from '../../lib/edgestore';
-
 
 const WritePost = () => {
     const [title, setTitle] = useState('');
@@ -17,6 +16,21 @@ const WritePost = () => {
     const [files, setFiles] = useState<File[]>([]);
     const { status } = useSession();
     const { edgestore } = useEdgeStore();
+    const [open, setOpen] = useState(false);
+    const [severityStatus, setSeverityStatus] = useState('');
+    const [snackMessage, setSnackMessage] = useState('');
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
+    const openSnackBar = (status: AlertColor, message: string) => {
+        setSeverityStatus(status);
+        setSnackMessage(message);
+        setOpen(true);
+    };
 
     useLayoutEffect(() => {
         if (status !== 'authenticated' && status !== 'loading') {
@@ -31,7 +45,6 @@ const WritePost = () => {
         setFiles([]);
     };
 
-    const saveAsDraft = () => console.log('saved as draft');
 
     const uploadPost = async (jsonData: string) => {
         const response = await fetch('/api/post', {
@@ -45,37 +58,43 @@ const WritePost = () => {
         return data.status;
     }
 
-    const publishPost = async () => {
+    const publishPost = async (publish: boolean) => {
         if (files?.[0] && title && content && category) {
             //upload image to edgestore server
-            const res = await edgestore.publicFiles.upload({
-                file: files[0],
-                options: {
-                    temporary: true,
-                },
-                onProgressChange: (progress) => {
-                    // you can use this to show a progress bar
-                    console.log(progress);
-                },
-            });
-            //upload post to database
-            const data = {
-                title,
-                content,
-                image: res.url,
-                category,
-                published: true,
-            };
-            const jsonData = JSON.stringify(data);
-            const resp = await uploadPost(jsonData);
-            if (resp === 200) {
-                await edgestore.publicFiles.confirmUpload({
-                    url: res.url,
+            try {
+                const res = await edgestore.publicFiles.upload({
+                    file: files[0],
+                    options: {
+                        temporary: true,
+                    },
+                    onProgressChange: (progress) => {
+                        // you can use this to show a progress bar
+                        console.log(progress);
+                    },
                 });
-                resetForm();
+                //upload post to database
+                const data = {
+                    title,
+                    content,
+                    image: res.url,
+                    category,
+                    published: publish,
+                };
+                const jsonData = JSON.stringify(data);
+                const resp = await uploadPost(jsonData);
+                if (resp === 200) {
+                    await edgestore.publicFiles.confirmUpload({
+                        url: res.url,
+                    });
+                    resetForm();
+                    openSnackBar(publish ? 'success' : 'info', publish ? 'Post published successfully' : 'Saved to draft')
+                } 
+            } catch {
+                openSnackBar('error', 'Something went wrong!')
             }
+            
         }
-        
+
     }
 
     return (
@@ -93,8 +112,8 @@ const WritePost = () => {
                     </div>
                     <div className='buttonsContainer'>
                         <Button onClick={resetForm} className='btn' id='btn1' variant="outlined">Reset</Button>
-                        <Button onClick={saveAsDraft} className='btn' id='btn2' variant="outlined">Save as draft</Button>
-                        <Button onClick={publishPost} className='btn' id='btn3' variant="contained" >Publish</Button>
+                        <Button onClick={() => publishPost(false)} className='btn' id='btn2' variant="outlined">Save as draft</Button>
+                        <Button onClick={() => publishPost(true)} className='btn' id='btn3' variant="contained" >Publish</Button>
                     </div>
                 </div>
                 <div className='rightPortion' >
@@ -110,6 +129,16 @@ const WritePost = () => {
                         </div>
                     </Paper>
                 </div>
+                <Snackbar         anchorOrigin={{ vertical: 'top', horizontal: 'right' }} open={open} autoHideDuration={3000} onClose={handleClose}>
+                    <Alert
+                        onClose={handleClose}
+                        severity={severityStatus}
+                        variant="filled"
+                        sx={{ width: '100%' }}
+                    >
+                        {snackMessage}
+                    </Alert>
+                </Snackbar>
             </div>
         </>
     )
